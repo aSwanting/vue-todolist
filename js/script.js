a9dc34676a39fa8133df85a83a0754464ba924f8
aSwanting <gab.damico@gmail.com>
2023-10-25 14:25:03 +0200

    add: basic html template, vue mounted

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+
+            tasks: [
+                
+                {
+                    taskName: "Task 1",
+                    taskDone: false,
+                },
+
+                {
+                    taskName: "Task 2",
+                    taskDone: false,
+                },
+
+                {
+                    taskName: "Task 3",
+                    taskDone: false,
+                },
+
+            ]
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log("It's Alive!")
+    }
+}).mount('#app')
